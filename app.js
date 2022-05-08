d3ecbe7260cdc51c3f5276942c041456f9c315de
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-05-08 18:27:13 +0300

    Refactor the code

--- app.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-const logo = document.querySelector('.logo-container');
-
-logo.addEventListener('click', handleToggle);
-
-function handleToggle(e) {
-  sidebar.classList.toggle('active');
-}
